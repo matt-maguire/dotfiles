7349b77721dcd3d11e731c6c76b6df2554f64b11
Matt Maguire <matthew.maguire@gmail.com>
2024-10-10 11:49:09 +1100

    Add doom config files

---
+++ .config/doom/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   '((output-pdf "PDF Tools")
+     (output-pdf "Evince")
+     (output-dvi "open")
+     (output-pdf "open")
+     (output-html "open")
+     (output-pdf "preview-pane"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
